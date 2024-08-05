90a404064da9a7ebd68713ba4e9e300bba8a87d0
Impartialll <maxutkom@gmail.com>
2024-08-06 01:43:32 +0300

    Ready to play! ... with yourself

--- main.c
+++ main.c
@@ -3,6 +3,27 @@
 #include <string.h>
 #include <stdbool.h>
 
+typedef struct Ball
+{
+  Vector2 position;
+  Vector2 speed;
+  float radius;
+} Ball;
+
+Vector2 GetRandomBallSpeed(int minSpeed, int maxSpeed)
+{
+  Vector2 speed = {
+      GetRandomValue(minSpeed, maxSpeed) * (GetRandomValue(0, 1) ? 1 : -1),
+      GetRandomValue(minSpeed, maxSpeed) * (GetRandomValue(0, 1) ? 1 : -1)};
+  return speed;
+}
+
+void ResetBall(Ball *ball, int screenWidth, int screenHeight)
+{
+  ball->position = (Vector2){screenWidth / 2.0f, screenHeight / 2.0f};
+  ball->speed = GetRandomBallSpeed(200, 400);
+}
+
 int main(void)
 {
   const char *title = "Ping pong";
@@ -10,38 +31,124 @@ int main(void)
   const int screenWidth = 640;
   const int screenHeight = 480;
 
-  // Circle speed
-  int circle_x = 140;
-  int speed = 5;
-  int direction = 1;
+  // Ball properties
+  Ball ball = {.position = {screenWidth / 2.0f, screenHeight / 2.0f},
+               .speed = GetRandomBallSpeed(200, 400),
+               .radius = 15.0f};
+
+  // Paddles properties
+  int paddleWidth = 20;
+  int paddleHeight = 100;
+  int leftPaddleX = 100;
+  int rightPaddleX = screenWidth - 120;
+
+  // Scores
+  int scoreLeft = 0;
+  int scoreRight = 0;
 
   int font_size = (screenWidth / screenHeight) * 50;
-  InitWindow(screenWidth, screenHeight, "Ping Pong Online");
+  InitWindow(screenWidth, screenHeight, "Pong Online");
   SetMouseCursor(MOUSE_CURSOR_ARROW);
   SetTargetFPS(60); // Set our game to run at 60 frames-per-second
 
+  // Calculate the top-left corner coordinates of the rectangle
+  int rectWidth = screenWidth / 2 + 200;
+  int rectHeight = screenHeight / 2 + 40;
+  int rectX = (screenWidth - rectWidth) / 2;
+  int rectY = (screenHeight - rectHeight) / 2;
+
+  int countdown = 3;
+  float countdownTimer = 0.0f;
+  bool gamePaused = true;
+
   while (!WindowShouldClose())
   {
-    // Calculate the top-left corner coordinates of the rectangle
-    int rectWidth = screenWidth / 2 + 200;
-    int rectHeight = screenHeight / 2 + 40;
-    int rectX = (screenWidth - rectWidth) / 2;
-    int rectY = (screenHeight - rectHeight) / 2;
-
     // Limit the player's rectangle Y position to stay within the larger rectangle
     Vector2 mouse_pos = GetMousePosition();
-    int player1_y = mouse_pos.y;
-    int player2_y = mouse_pos.y;
+    int leftPaddleY = mouse_pos.y - paddleHeight / 2;
+    int rightPaddleY = mouse_pos.y - paddleHeight / 2;
 
-    if (player1_y < rectY)
-      player1_y = rectY;
-    if (player1_y + 100 > rectY + rectHeight)
-      player1_y = rectY + rectHeight - 100;
+    if (leftPaddleY < rectY)
+      leftPaddleY = rectY;
+    if (leftPaddleY + paddleHeight > rectY + rectHeight)
+      leftPaddleY = rectY + rectHeight - paddleHeight;
 
-    if (player2_y < rectY)
-      player2_y = rectY;
-    if (player2_y + 100 > rectY + rectHeight)
-      player2_y = rectY + rectHeight - 100;
+    if (rightPaddleY < rectY)
+      rightPaddleY = rectY;
+    if (rightPaddleY + paddleHeight > rectY + rectHeight)
+      rightPaddleY = rectY + rectHeight - paddleHeight;
+
+    // Concole output
+    // printf("x: %f y: %f\n", mouse_pos.x, mouse_pos.y);
+
+    if (gamePaused)
+    {
+      countdownTimer += GetFrameTime();
+      if (countdownTimer >= 1.0f)
+      {
+        countdownTimer = 0.0f;
+        countdown--;
+        if (countdown <= 0)
+        {
+          gamePaused = false;
+        }
+      }
+    }
+    else
+    {
+      // Ball movement
+      ball.position.x += ball.speed.x * GetFrameTime();
+      ball.position.y += ball.speed.y * GetFrameTime();
+
+      // Ball collision with top and bottom screen boundaries
+      if (ball.position.y <= ball.radius || ball.position.y >= screenHeight - ball.radius)
+      {
+        ball.speed.y *= -1;
+      }
+
+      // Ball collision with paddles
+      if (ball.position.x - ball.radius <= leftPaddleX + paddleWidth &&
+          ball.position.y >= leftPaddleY &&
+          ball.position.y <= leftPaddleY + paddleHeight)
+      {
+        ball.speed.x *= -1;
+        ball.speed.y += (ball.position.y - (leftPaddleY + paddleHeight / 2)) * 2;
+      }
+
+      if (ball.position.x + ball.radius >= rightPaddleX &&
+          ball.position.y >= rightPaddleY &&
+          ball.position.y <= rightPaddleY + paddleHeight)
+      {
+        ball.speed.x *= -1;
+        ball.speed.y += (ball.position.y - (rightPaddleY + paddleHeight / 2)) * 2;
+      }
+
+      // Ball collision with center rectangle boundaries
+      if (ball.position.x - ball.radius < rectX || ball.position.x + ball.radius > rectX + rectWidth)
+      {
+        ball.speed.x *= -1;
+      }
+      if (ball.position.y - ball.radius < rectY || ball.position.y + ball.radius > rectY + rectHeight)
+      {
+        ball.speed.y *= -1;
+      }
+
+      // Victory!
+      if (ball.position.x + ball.radius > rightPaddleX + paddleWidth)
+      {
+        scoreLeft++;
+        gamePaused = true;
+        countdown = 3;
+        ResetBall(&ball, screenWidth, screenHeight);
+      }
+      else if (ball.position.x - ball.radius < leftPaddleX)
+      {
+        scoreRight++;
+        gamePaused = true;
+        countdown = 3;
+        ResetBall(&ball, screenWidth, screenHeight);
+      }
+    }
 
     BeginDrawing();
     ClearBackground(LIGHTGRAY);
@@ -56,29 +163,34 @@ int main(void)
     DrawText(footer, (screenWidth - MeasureText(footer, 14)) / 2,
              screenHeight - 35, 14, BLACK);
 
-    // Concole output
-    printf("x: %f y: %f\n", mouse_pos.x, mouse_pos.y);
+    // Draw ball
+    DrawCircleV(ball.position, ball.radius, BLACK);
 
-    // Circle movement
-    circle_x += speed * direction;
-    if (circle_x >= screenWidth - 140 || circle_x <= 140)
-    {
-      direction *= -1;
-    }
-    DrawCircle(circle_x, screenHeight / 2, 20.0f, BLACK);
-
-    // Marking players
+    // Draw players
     if ((int)mouse_pos.x <= screenWidth / 2)
     {
-      printf("Player 1 active: ");
-      DrawRectangle(100, player1_y, 20, 100, Fade(GREEN, 0.9f));
+      // printf("Player 1 active: ");
+      DrawRectangle(100, leftPaddleY, 20, 100, Fade(GREEN, 0.9f));
       DrawRectangle(screenWidth - 120, (screenHeight - 100) / 2, 20, 100, Fade(RED, 0.9f));
     }
     else
     {
-      printf("Player 2 active: ");
-      DrawRectangle(screenWidth - 120, player2_y, 20, 100, Fade(RED, 0.9f));
-      DrawRectangle(100, (screenHeight - 100) / 2, 20, 100, Fade(GREEN, 0.9f));
+      // printf("Player 2 active: ");
+      DrawRectangle(rightPaddleX, rightPaddleY, paddleWidth, paddleHeight, Fade(RED, 0.9f));
+      DrawRectangle(leftPaddleX, (screenHeight - 100) / 2, paddleWidth, paddleHeight, Fade(GREEN, 0.9f));
+    }
+
+    // Draw scores
+    DrawText(TextFormat("Left: %i", scoreLeft), 70, 70, 20, BLACK);
+    DrawText(TextFormat("Right: %i", scoreRight), screenWidth - 140, 70, 20, BLACK);
+
+    // Draw countdown
+    if (gamePaused && countdown > 0)
+    {
+      const char *countdownText = TextFormat("%i", countdown);
+      int textWidth = MeasureText(countdownText, 60);
+      DrawRectangle((screenWidth / 2) - (textWidth / 2) - 10, screenHeight / 2 - (60 / 2) - 10, textWidth + 20, 60 + 20, LIGHTGRAY);
+      DrawText(countdownText, (screenWidth / 2) - (textWidth / 2), screenHeight / 2 - (60 / 2), 60, BLACK);
     }
 
     EndDrawing();
